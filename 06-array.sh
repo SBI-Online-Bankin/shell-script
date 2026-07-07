efb754abe57b351b8dd45ded846f929375aed7d2
Ram-Indla <ramakrishna.aws2026@gmail.com>
2026-07-07 17:25:40 +0530

    added new 06-vaariables.sh file

--- 06-array.sh
+++ 06-array.sh
@@ -2,3 +2,6 @@
 MOVIES=("RRR" "PEDDHI" "OG")  
 
 echo "My first movie is : ${MOVIES[0]}"
+echo "My second movie is : ${MOVIES[1]}"
+echo "My Third movie is : ${MOVIES[2]}"
+echo "My all movies is : ${MOVIES[@]}" # @ is symbol of all. we can use * aswell
