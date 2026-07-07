4e974d43915a6ddc554f16a4ade5e1e82fcd4588
Ram-Indla <ramakrishna.aws2026@gmail.com>
2026-07-07 17:06:25 +0530

    added new 04-vaariables.sh file

--- 05-variables.sh
+++ 05-variables.sh
@@ -3,6 +3,6 @@ echo "please enter the username::"
 read USERNAME
 
 echo "Please enter password"
-read PASSWORD
+read -s PASSWORD #here -s means secret
 
 echo "USERNAME is : $USERNAME, PASSWORD is : $PASSWORD"
