d9a0b29dcff482c1e3530946e0e7dcfa0f0dc773
Ram-Indla <ramakrishna.aws2026@gmail.com>
2026-07-08 23:00:07 +0530

    modified the 11-installations.sh file

--- 11-functions.sh
+++ 11-functions.sh
@@ -4,24 +4,15 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
     echo "Please try with super user"
-    exit 1    #Manually exit
+    exit 1    # here "exit 1" is Manually exit
 else
     echo "You are super user"
 fi
 
-validate-function(){
-
-echo "Installing $2"
-dnf install $2 -y
-if [ $? -ne 0 ]
-then
-    echo "Instalation of mysql...failure"
-    exit 1
-else
-    echo "installation of my sql is success"
-fi
-
+validate_function(){
+    echo "Exit status $1"
+    echo "Package is $2"
 }
 
-validate-function $? $Package
+validate_function $? "mysql"
 
